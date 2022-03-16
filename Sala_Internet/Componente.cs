5685ae2da96376c90f6d833b9f32e718d1a73d5f
ralex <ralex@DESKTOP-2IVAEDJ>
2022-03-16 23:03:15 +0200

    Add project files.

---
+++ Sala_Internet/Componente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sala_Internet
+{
+	class Componente
+	{
+		public int RAM;
+		public string CPU;
+		public string GPU;
+		public string cooler;
+		public string HDD;
+		public string SSD;
+
+		public Componente(int ram, string procesor, string placa, string cool, string HardDisk, string SolidDrive)
+		{
+			RAM = ram;
+			CPU = procesor;
+			GPU = placa;
+			cooler = cool;
+			HDD = HardDisk;
+			SSD = SolidDrive;
+		}
+
+	}
+}
